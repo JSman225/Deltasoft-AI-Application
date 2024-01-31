c2c2da8fcd3931a713d453890e6db6bea39e87da
JSman225 <84781179+JSman225@users.noreply.github.com>
2024-01-31 14:27:13 -0500

    test

--- app/components/chatInput/page.js
+++ app/components/chatInput/page.js
@@ -21,14 +21,9 @@ export default function ChatInput({ conversation, setConversation }) {
         Refresh(setConversation);    
     };
     return (
-        <div className="bg-white border border-gray-300 shadow-md rounded-2xl w-full max-w-4xl p-4 mx-auto flex flex-row gap-4 items-center">
-            <img className="w-8 h-8 rounded-full bg-gray-300" src={user.imageUrl} alt={user.name} />
-            <input onChange={handleSearchChange} value={content} className="overflow-y-scroll outline-none resize-none border-none w-full text-neutral-700 font-medium text-base leading-6" placeholder="Chat with Delta..." />
-            <div onClick={onClick} className="text-2x self-end w-6 h-6 m-1 text-gray-600 transition-colors cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                </svg>
-            </div>
-        </div>
+<div className="bg-white border border-gray-300 shadow-md rounded-2xl w-full max-w-4xl p-4 mx-auto flex flex-row gap-4 items-center"> 
+<textarea onChange={handleSearchChange} value={content} className="overflow-y-scroll outline-none resize-none border-none w-full text-neutral-700 font-medium text-base leading-6" placeholder="Chat with Delta..." onKeyDown={(e) => { if (e.key === 'Enter' && e.shiftKey) { e.preventDefault();  } 
+}} /> 
+</div>
     );
 }
